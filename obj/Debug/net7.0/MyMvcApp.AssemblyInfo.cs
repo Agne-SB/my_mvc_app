e23a45dd9db02cb4c3a3b40de028a3aba67ce099
Agne Burikaite <agne@Agnes-MacBook-Pro.local>
2025-03-24 23:02:41 +0100

    Fix DB connection hostname

--- obj/Debug/net7.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8547678f8ab11007fccea1fb7b7f3bd734233b6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a52c76aca807ec671946372ce853636915cf82")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
